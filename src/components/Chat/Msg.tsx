250a284b3c096d9a794c849b9d28e2cb2719c3d4
BarBronshtein <barbronshtein@gmail.com>
2022-11-27 18:26:23 +0200

    added markup to Msg component

--- src/components/Chat/Msg.tsx
+++ src/components/Chat/Msg.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+type Props = {
+	type?: 'video' | 'img';
+	url?: string;
+	txt: string;
+};
 
-const Msg = () => {
-	return <div></div>;
+const Msg = (props: Props) => {
+	const urlRegex =
+		/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+	const words = props.txt.split(' ');
+
+	return (
+		<div className="msg max-w-[300]">
+			<p>
+				{words.map(word =>
+					word.match(urlRegex) ? <a href={word}>{word} </a> : word + ' '
+				)}
+			</p>
+		</div>
+	);
 };
 
 export default Msg;
--- src/components/ChatList/ChatListHeader.tsx
+++ src/components/ChatList/ChatListHeader.tsx
@@ -4,13 +4,13 @@ const ChatListHeader = () => {
 	const [open, setOpen] = useState(false);
 	return (
 		<header className="flex">
-			<button
-				onClick={() => setOpen(prev => !prev)}
-				className="hamburger-icon"
-			></button>
+			<span
+				onClick={() => setOpen(() => true)}
+				className="fa-regular fa-bars"
+			></span>
 			<h1>Chats</h1>
-			<button className="pen"></button>
-			<SideBar isShown={open} onClose={() => setOpen(prev => false)} />
+			<span className="fa-solid fa-pen"></span>
+			<SideBar isShown={open} onClose={() => setOpen(() => false)} />
 		</header>
 	);
 };
