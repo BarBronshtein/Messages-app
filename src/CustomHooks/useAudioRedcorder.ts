44cc03355b0c748acc970796f77bb7719a8ac2ca
BarBronshtein <barbronshtein@gmail.com>
2022-12-08 20:09:27 +0200

    added custom hook

---
+++ src/CustomHooks/useAudioRedcorder.ts
@@ -0,0 +1,55 @@
+import { useState, useRef } from 'react';
+
+enum RECORD_STATUS {
+	RECORDING = 'recording',
+	IDLE = 'idle',
+}
+
+export const useAudioRecorder = () => {
+	let mediaRecorder: MediaRecorder;
+	let localStream: MediaStream;
+
+	const dataArray = useRef<Blob[]>([]);
+	const [status, setStatus] = useState<RECORD_STATUS>(RECORD_STATUS.IDLE);
+	const [audioResult, setAudioResult] = useState<Blob>(null!);
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const startRecording = () => {
+		if (status !== RECORD_STATUS.IDLE) return;
+		navigator.mediaDevices
+			.getUserMedia({ audio: true })
+			.then((mediaStream: MediaStream) => {
+				localStream = mediaStream;
+				mediaRecorder = new MediaRecorder(mediaStream);
+				mediaRecorder.start();
+
+				mediaRecorder.onstart = () => setStatus(RECORD_STATUS.RECORDING);
+
+				mediaRecorder.ondataavailable = ({ data }: BlobEvent) =>
+					dataArray.current.push(data);
+			})
+			.catch(err => setErrorMessage(err));
+	};
+
+	const stopRecording = () => {
+		if (status === RECORD_STATUS.IDLE) return;
+		mediaRecorder.stop();
+
+		mediaRecorder.onstop = () => {
+			const audioData = new Blob(dataArray.current, { type: 'audio/webm;' });
+			dataArray.current = [];
+			setAudioResult(audioData);
+			setStatus(RECORD_STATUS.IDLE);
+			localStream
+				.getAudioTracks()
+				.forEach((track: MediaStreamTrack) => track.stop());
+		};
+	};
+	return {
+		startRecording,
+		stopRecording,
+		audioResult,
+		errorMessage,
+		status,
+	};
+};
