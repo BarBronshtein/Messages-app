e8b6dc5b0cdac3e51a1385306ae9c3567d3c64de
BarBronshtein <barbronshtein@gmail.com>
2023-02-14 12:02:33 +0200

    added interface in vite-env.d file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,3 +10,14 @@ interface Window {
 	gStore: Store;
 	__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 }
+interface ImportMetaEnv {
+	readonly VITE_APP_TITLE: string;
+	readonly VITE_REMOTE_ASSETS_APP_URL: string;
+	readonly VITE_REMOTE_APP_URL: string;
+
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
