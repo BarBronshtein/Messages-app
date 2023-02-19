0287b3076c514723edcbd3f775f13a0c76f317d4
BarBronshtein <97507642+BarBronshtein@users.noreply.github.com>
2023-02-19 12:09:58 +0000

    Deploying to gh-pages from @ BarBronshtein/Messages-app@ba52a14eedda7c83ff8d61f0a17c1d9e21c018b2 🚀

--- assets/Login.7e098f74.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_virtual___federation__.649c7e16.js";import{r as o,b as r}from"./index.edbdaee8.js";const n=await e("auth","./Login");let{LoginForm:a}=n;const f=()=>{const t=o.exports.useRef(null);return o.exports.useEffect(()=>{a(t.current)},[]),r("div",{ref:t})};export{f as default};
